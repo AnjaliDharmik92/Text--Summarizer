da241c10370267730f09254cd0f342ad9e106de2
AnjaliDharmik92 <123653314+AnjaliDharmik92@users.noreply.github.com>
2023-08-05 02:32:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def predict():
 
         my_prediction = ""#pipe(News_Article, **gen_kwargs)[0]
               
-        return render_template('result.html', summary = my_prediction)
+        return render_template('result.html')#, summary = my_prediction)
 
 
 if __name__ == '__main__':
