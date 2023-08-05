dc21932855c5d646673f49e55ac791ee3748e8d2
AnjaliDharmik92 <123653314+AnjaliDharmik92@users.noreply.github.com>
2023-08-05 11:18:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def predict():
        
         text_to_summarize = request.form.get('text_to_summarize')
 
-        my_prediction = pipe(text_to_summarize, **gen_kwargs)[0]
+        my_prediction = ""#pipe(text_to_summarize, **gen_kwargs)[0]
               
         return render_template('result.html'), summary = my_prediction)
 
